f3dd4ea452f293c761d8633d0bf2c6e36356f714
Laurens <497234@student.saxion.nl>
2022-03-04 13:49:06 +0100

    base grid component with some styling and effects

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	import Grid from './components/Grid.svelte';
+
+	let maze = [
+        [0,0,0,0,1],
+        [0,0,0,0,0],
+        [0,1,1,1,0],
+        [0,0,0,0,0],
+        [1,0,0,0,0]
+        ]
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Grid {maze} />
 </main>
 
 <style>
 	main {
+		background-color: blue;
+		border-radius: 1rem;
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	
 </style>
---
+++ src/components/Grid.svelte
@@ -0,0 +1,58 @@
+<script>
+    export let maze = [
+        [0,0,0,0,1],
+        [0,0,0,0,0],
+        [0,0,1,0,0],
+        [0,0,0,0,0],
+        [1,0,0,0,0]
+        ]
+
+</script>
+
+<div class="container">
+    {#each maze as row}
+        <div class="row">
+            {#each row as col}
+                <div class="col col_{col}"></div>
+            {/each}
+        </div>
+    {/each}
+</div>
+
+
+<style>
+    .container {
+        padding: 0.5em;
+        border-radius: 1rem;
+        background-color: rgb(72, 98, 107);
+    }
+
+    .row { 
+        display: flex;
+    }
+
+    .col {
+        flex-grow: 1;
+        aspect-ratio: 1;
+        border-radius: 1rem;
+        transition: border-radius .1s, filter .25s;
+    }
+
+    .col:hover {
+        border-radius: .75rem;
+        filter: brightness(0.85);
+    }
+
+    .col:active {
+        border-radius: 50%;
+    }
+
+    .col_1 {
+        background-color: rgb(48, 58, 59);
+    }
+
+    .col_0 {
+        background-color: rgb(217, 235, 238);
+    }
+
+</style>
